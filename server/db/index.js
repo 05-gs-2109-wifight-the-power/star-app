46d89c14d1d503f0fba4ea61d68ee85be1c9df81
Jordan Walters <jwalters012@gmail.com>
2021-11-10 15:59:09 -0500

    cart and associations complete

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,26 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require("./db");
 
-const User = require('./models/User')
+const User = require("./models/User");
+const Cart = require("./models/Cart");
+
+// fix
+const Star = require("./models/Star");
 
 //associations could go here!
+
+//check spelling of Star...
+//quotes or no quotes on cart
+User.belongsToMany(Star, { through: Cart });
+Star.belongsToMany(User, { through: Cart });
 
 module.exports = {
   db,
   models: {
     User,
+    Cart,
+    //stars/star?
+    Star,
   },
-}
+};
