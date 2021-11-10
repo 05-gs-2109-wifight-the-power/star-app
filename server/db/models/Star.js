0a1699d0782a5bcaff2d522cb7cd3d25508ba8e5
Gi Diaz <yandelydf@gmail.com>
2021-11-10 12:29:02 -0500

    First commit

---
+++ server/db/models/Star.js
@@ -0,0 +1,15 @@
+// const Sequelize = require("sequelize");
+// const db = require("../db");
+
+// const Star = db.define("star", {
+//   name: {
+//     type: Sequelize.STRING,
+//     allowNull: false,
+//     validate: {
+//       notEmpty: true
+//     }
+//   },
+//   coordinates
+// });
+
+// module.exports = Star;
