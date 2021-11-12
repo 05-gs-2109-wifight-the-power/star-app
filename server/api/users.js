0c1a346c3e61634875060e6178722e1bf31f9fc7
Margaret Farmer <margaret.farmer@gmail.com>
2021-11-12 14:03:02 -0500

    Fix: updated security

--- server/api/users.js
+++ server/api/users.js
@@ -2,26 +2,7 @@ const router = require('express').Router()
 const { models: { User }} = require('../db')
 module.exports = router
 
-const requireToken = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    const user = await User.findByToken(token);
-
-    req.user = user
-    next()
-  } catch (e) {
-    next(e)
-  }
-}
-
-const isAdmin = (req, res, next) => {
-  if(!req.user.isAdmin) {
-    return res.status(403).send('Thou shalt not pass')
-    // if user is admin, pass them forward
-  } else {
-    next()
-  }
-}
+const { requireToken, isAdmin } = require('./gateKeepingMiddleware')
 
 router.get('/', requireToken, isAdmin, async (req, res, next) => {
   try {
--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const morgan = require('morgan')
 const app = express()
 module.exports = app
 
+if (process.env.NODE_ENV !== 'development') {
+  require('../secrets')
+  //console.log('log environment variables', process.env)
+}
+
+const SECRET_API_KEY = process.env.SECRET_API_KEY
+
+
 // logging middleware
 app.use(morgan('dev'))
 
