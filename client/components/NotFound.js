2455f7fb274c87ccb6aaea9611587146c1663a09
Gi Diaz <yandelydf@gmail.com>
2021-11-14 15:50:20 -0500

    Refactor: Updated NotFound with React-Icons

--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { GrNext } from "react-icons/gr";
 
 export default class NotFound extends Component {
   render() {
     return (
-      <div className="backgroundImg">
+      <div className="backgroundImg" style={{ textAlign: "center" }}>
         <div className="notFound">404</div>
         <div className="ntFound-content" style={{ textAlign: "center" }}>
           <h1>Oh No, Star Not Found!</h1>
-          <h3>Looks like this star has not been discovered yet.</h3>
-          <Link>
-            <h2 className="backLink">--{">"} Back to the stars</h2>
+          <h3>Looks like this Star has not been discovered yet.</h3>
+          <Link to="/stars">
+            <h2 className="backLink">
+              <GrNext /> Back to the Stars
+            </h2>
           </Link>
         </div>
       </div>
