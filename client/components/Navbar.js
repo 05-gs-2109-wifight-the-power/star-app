a77e6dff3f4df0044eda09c5cf9ec847d6fbf3ed
Gi Diaz <yandelydf@gmail.com>
2021-11-16 15:00:11 -0500

    Refactor: Updating Sign In

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,36 +1,90 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 import { Login, Signup } from "./AuthForm";
 
-const Navbar = ({ handleClick, isLoggedIn, username }) => (
-  <div>
-    <h1>ASTRO MART</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/stars">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <h3>Welcome, {username}</h3>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link>Sign in</Link>
-          <Login />
-          {/* <Signup /> */}
-          {/* <Link to="/login">Login</Link>
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: true,
+    };
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle() {
+    this.setState({
+      hidden: !this.state.hidden,
+    });
+  }
+
+  render() {
+    const { handleClick, isLoggedIn } = this.props;
+    const { handleToggle } = this;
+    return (
+      <div>
+        <h1>ASTRO MART</h1>
+        <nav>
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/stars">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+              <h3>Welcome, {username}</h3>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <button onClick={handleToggle}>Sign in</button>
+              {!this.state.hidden && <Login />}
+              {/* <div className="hidden">
+                <Login />
+              </div> */}
+
+              {/* <Signup /> */}
+              {/* <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link> */}
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-);
+            </div>
+          )}
+        </nav>
+        <hr />
+      </div>
+    );
+  }
+}
+// const Navbar = ({ handleClick, isLoggedIn, username }) => (
+//   <div>
+//     <h1>ASTRO MART</h1>
+//     <nav>
+//       {isLoggedIn ? (
+//         <div>
+//           {/* The navbar will show these links after you log in */}
+//           <Link to="/stars">Home</Link>
+//           <a href="#" onClick={handleClick}>
+//             Logout
+//           </a>
+//           <h3>Welcome, {username}</h3>
+//         </div>
+//       ) : (
+//         <div>
+//           {/* The navbar will show these links before you log in */}
+//           <button onClick={() => toggleSignIn()}>Sign in</button>
+//           <div className="hidden">
+//             <Login />
+//           </div>
+
+//           {/* <Signup /> */}
+//           {/* <Link to="/login">Login</Link>
+//           <Link to="/signup">Sign Up</Link> */}
+//         </div>
+//       )}
+//     </nav>
+//     <hr />
+//   </div>
+// );
 
 /**
  * CONTAINER
