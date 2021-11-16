53dc6622f5b5213b0ae4114b623752a23fdb7b92
Gi Diaz <yandelydf@gmail.com>
2021-11-16 15:49:53 -0500

    Task: Thank you for your purchase

---
+++ client/components/ThankYou.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ThankYou = () => {
+  return (
+    <div>
+      <img src="/images/cart.png" width="100px" />
+      <h5>Thank you for your purchase</h5>
+    </div>
+  );
+};
+
+export default ThankYou;
