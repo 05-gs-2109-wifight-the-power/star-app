d1b7cac27b8f60a77b076a12dad2d0e384119116
Sejung <49921709+sejungk@users.noreply.github.com>
2021-11-18 10:29:45 -0500

    style: cart styling

--- client/components/ThankYou.js
+++ client/components/ThankYou.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const ThankYou = () => {
   return (
-    <div>
+    <div className="thanks-wrapper">
       <img src="/images/cart.png" width="100px" />
-      <h5>Thank you for your purchase</h5>
+      <h5 className="thanks-text">Thank you for your purchase</h5>
     </div>
   );
 };
